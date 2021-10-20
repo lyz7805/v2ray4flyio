90cdac7a0dd557a66a8ec5add75346b55af633ee
lyz7805 <lyz7805@126.com>
2021-10-20 06:12:07 +0000

    优化fly.toml应用配置

--- deploy.sh
+++ deploy.sh
@@ -32,28 +32,38 @@ app = "$APP_NAME"
 
 kill_signal = "SIGINT"
 kill_timeout = 5
+processes = []
+
+[env]
+
+[experimental]
+  allowed_public_ports = []
+  auto_rollback = true
 
 [[services]]
+  http_checks = []
   internal_port = 443
+  # processes = ["app"]
   protocol = "tcp"
+  script_checks = []
 
   [services.concurrency]
-    hard_limit = 25
-    soft_limit = 20
+    hard_limit = 50
+    soft_limit = 35
+    type = "connections"
 
   [[services.ports]]
     handlers = ["http"]
-    port = "80"
+    port = 80
 
   [[services.ports]]
     handlers = ["tls", "http"]
-    port = "443"
+    port = 443
 
   [[services.tcp_checks]]
-    grace_period = "1s"
+    grace_period = "120s"
     interval = "15s"
-    port = "8080"
-    restart_limit = 6
+    restart_limit = 0
     timeout = "2s"
 EOF
 printf '\e[32mCreate app config file success.\n\e[0m'
