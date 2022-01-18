e562a13432b3618062579dbe27dc01afad31ef60
lyz7805 <lyz7805@126.com>
2022-01-18 14:40:29 +0800

    fix #7
    修复 #7

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,13 @@ if [ -z "${APP_NAME}" ]; then
     printf '\e[31mPlease set APP_NAME first.\n\e[0m' && exit 1
 fi
 
-if [ "$(flyctl info --app "${APP_NAME}" | grep -o "Could not resolve App")" = "Could not resolve App" ]; then
+flyctl info --app "${APP_NAME}" >/tmp/${APP_NAME} 2>&1;
+if [ "$(cat /tmp/${APP_NAME} | grep -o "Could not resolve App")" = "Could not resolve App" ]; then
     printf '\e[33mCould not resolve app. Next, create the App.\n\e[0m'
     flyctl apps create "${APP_NAME}"
 
-    if [ "$(flyctl info --app "${APP_NAME}" | grep -o "Could not resolve App")" != "Could not resolve App" ]; then
+    flyctl info --app "${APP_NAME}" >/tmp/${APP_NAME} 2>&1;
+    if [ "$(cat /tmp/${APP_NAME} | grep -o "Could not resolve App")" != "Could not resolve App" ]; then
         printf '\e[32mCreate app success.\n\e[0m'
     else
         printf '\e[31mCreate app failed.\n\e[0m' && exit 1
